df6893eb6af081d52f6088443cbdd1b9a3833e97
USER <younghwan.kim@navercorp.com>
2023-03-24 00:43:19 +0900

    spring batch guide
    guide : https://spring.io/guides/gs/batch-processing/
    
    write code with kotlin

---
+++ study-spring-guide/batch/src/test/kotlin/me/clevekim/springguide/batch/BatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.clevekim.springguide.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ study-spring-guide/src/main/kotlin/me/clevekim/studyspringguide/StudySpringGuideApplication.kt
@@ -0,0 +1,11 @@
+package me.clevekim.studyspringguide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringGuideApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringGuideApplication>(*args)
+}
